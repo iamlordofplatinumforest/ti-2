cc0f131ae6960b8c38c5d5a4830596c11110cf37
Yefrosinya <efrosinasologub@gmail.com>
2025-03-26 18:12:11 +0300

    done

---
+++ src/main/java/com/example/lab2java/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.lab2java;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(GUI.class, args);
+    }
+}
